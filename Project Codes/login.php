8664b4a6da767c1b10cf7d6d8e79fc4ee626830d
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-07-20 22:51:20 +0600

    Feature : Login.php Form added

--- Project Codes/login.php
+++ Project Codes/login.php
@@ -2,9 +2,43 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <title>Log In</title>
 </head>
 <body>
     
+
+<div class="container">
+    
+    <div class="header">
+        <h2>Log In</h2>
+    </div>
+
+    <form action="registration.php" method="post">
+        
+        <div>
+            <label for="username">User Name : </label>
+            <input type="text" name="username"  required>
+        </div>
+
+       
+
+         <div>
+            <label for="password">Password : </label>
+            <input type="password" name="password_1" required>
+        </div>
+
+         
+
+         
+
+        <button type="submit" name="login_user">Log In</button>
+
+        <p>Already a user <a href="registration.php"><b>Register Here</b></a></p>
+
+    </form>
+
+</div>
+
+
 </body>
 </html>

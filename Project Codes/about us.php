f98f7aef2a2e3b3bc6a64e37f251d3da36dbc376
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-07-22 00:26:56 +0600

    Frature: About us page

---
+++ Project Codes/about us.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>About Us</title>
+  </head>
+    <body>
+       <?php include ('header.php'); ?>
+  
+      <h1><span class="bold">About Our Organisation</span></h1><br><br>
+      <p> 
+
+      </p>
+    
+      
+       
+</body>
+</html>

e7910d7954da57c2af44ee90760d8c3ab8a5b734
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-07-22 00:34:01 +0600

    Feature: update on contact us

---
+++ Project Codes/contact_us.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Contact Us</title>
+  </head>
+    <body>
+       <?php include ('header.php'); ?>
+  
+			<h1><span class="bold">Contact us </span></h1><br><br>
+      <p> 
+         <font sizr="5">For more information:</font><br>
+         <font sizr="4">Call us : 017********</font><br>
+         <font sizr="4">Email us : onlinebloodbank@******.com</font><br>
+         <font sizr="4">Facebook Page: www.facebook.com/onlinebloodbank</font><br>
+
+      </p>
+		
+      
+       
+</body>
+</html>

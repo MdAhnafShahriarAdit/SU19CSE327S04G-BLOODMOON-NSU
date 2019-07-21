e371723aa298e02e4921a39a487fe062c77faf3d
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-07-21 21:16:46 +0600

    Feature : create send req file
    This part will be used for send request for blood donation

---
+++ Project Codes/send_req.php
@@ -0,0 +1,14 @@
+<! doctype html>
+<html lang="en">
+ <head>
+ 	<title>Online Blood Bank</title>
+ </head>
+ <body>
+ 	
+<?php include ('header.php'); ?>
+
+ </body>
+
+
+<?php include ('footer.php'); ?>
+</html>

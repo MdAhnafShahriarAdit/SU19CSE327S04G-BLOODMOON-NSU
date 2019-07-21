3c1b9ef9f622f4ef9c1dfa0cca7da17e11e95b46
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-07-22 01:55:37 +0600

    Form validation

--- Project Codes/server.php
+++ Project Codes/server.php
@@ -11,3 +11,17 @@ session_start();
 
   $db = mysqli_connect('localhost','root','','menstrualcycle') or die("could not connect to data base");
 
+//Register users 
+
+  $username =mysqli_real_escape_string($db, $_POST['username']);
+  $email =mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 =mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 =mysqli_real_escape_string($db, $_POST['password_2']);
+
+
+
+  // form validation 
+   if (empty($username)) {array_push($errors, "Username is required");}
+   if (empty($email)) {array_push($errors, "Email is required");}
+   if (empty($password_1)) {array_push($errors, "Password is required");}
+   if($password_1 != $password_2){array_push($errors,"Passwords need to be the same");}

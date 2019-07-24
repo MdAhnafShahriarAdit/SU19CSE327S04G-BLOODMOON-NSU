05cc5a77a2cc5696f0a4f21445cd5362e6a744db
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-07-24 19:00:31 +0600

    update about_us page

---
+++ Project Codes/about_us.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>About Us</title>
+  </head>
+    <body>
+       <?php include ('header.php'); ?>
+  
+      <h1><span class="bold">About Our Organisation</span></h1><br><br>
+      
+    <p> 
+    	Welcome to our online blood bank system.Our Website is Completely non-profit, non-political and
+        A serviceable Website.Its an attempt to work for humanity.If you are a donor,We appreciate you signing up online as a Donor.
+        If you need blood we are happy to serve you.We believe if you want to help someone its better to give blood and save life.
+      </p>
+      
+       
+</body>
+</html>

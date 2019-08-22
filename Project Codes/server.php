c7ce19ff656fb8898af3c4e8ef6049e7868faf8e
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-22 12:45:28 +0600

    Update server.php

--- Project Codes/server.php
+++ Project Codes/server.php
@@ -50,7 +50,7 @@ session_start();
   	$_SESSION['username']=$username;
   	$_SESSION['success']= "You are now logged in";
 
-  	header('location: index.php');
+  	header('location: Donor/index.php');
   }
 
 

83059ec7ad418b17ce6c3b86c958727760c71daf
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-22 21:22:52 +0600

    Create donate_us.php

---
+++ Project Codes/Donor/donate_us.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+<body>
+  <div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 700px; ">
+      <div class="content"><br>
+  <h1 align="center">Donate us to give us the opportunity to grow more</h1><br><br><br>
+
+ <p align="center"><font size="5">For more informtion:</font><br>
+   <font size="4">Bkash no: 017*******</font><br>
+   <font size="4">Rocket no:017*******</font><br>
+ </p>
+     <table width="100%" style="margin: 2 auto; border:3px solid;text-align:center">

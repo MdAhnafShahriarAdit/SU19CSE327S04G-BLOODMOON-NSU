78a9d58da8e1b02b1ab91b134de70e2e8679d1ce
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-22 12:44:47 +0600

    Update login.php

--- Project Codes/login.php
+++ Project Codes/login.php
@@ -2,27 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Log In</title>
+    <title>Onine Blood Bank</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
+<body >
+
+    <div class="header">
+    <div class="logo">
+              <h1><a href="index.php"><img src="Image/2.jpg" alt=""></a></h1>
+            
+            </div>
+  </div>
+
     
 
 <div class="container">
     
+
+
     <div class="header">
         <h2>Log In</h2>
     </div>
 
     <form action="registration.php" method="post">
+        <center><img src="img/l2.png" alt="#" class="l1"></center>
         
-        <div>
+        <div class="input-group">
             <label for="username">User Name : </label>
-            <input type="text" name="username"  required>
+            <input type="text" name="username" required>
         </div>
 
        
 
-         <div>
+         <div class="input-group">
             <label for="password">Password : </label>
             <input type="password" name="password_1" required>
         </div>
@@ -31,7 +43,9 @@
 
          
 
-        <button type="submit" name="login_user">Log In</button>
+        <div class="input-group">
+    <button type="submit" class="btn" name="login_user">Log In</button>
+        </div>
 
         <p>Already a user <a href="registration.php"><b>Register Here</b></a></p>
 

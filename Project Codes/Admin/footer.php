e2bb1bcd6816c2c3975b11bc788a996b62620915
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-22 21:44:53 +0600

    Create footer.php

---
+++ Project Codes/Admin/footer.php
@@ -0,0 +1,14 @@
+<style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #4c77ed ;
+  color: white;
+  text-align: center;
+}
+</style>
+<div class="footer">
+	<p>@ Group Blood_Moon</p>
+</div>

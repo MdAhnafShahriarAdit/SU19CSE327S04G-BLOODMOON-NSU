1a59dcd33c1be526c1cd78dd752385cdc1e786ed
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-22 12:25:04 +0600

    Update view request page

--- Project Codes/view_req.php
+++ Project Codes/view_req.php
@@ -3,9 +3,7 @@
     <title>View Request</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark" style="background-color: #f05656 !important;">
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
   <a class="navbar-brand" href="index.php" style="color: #1a1919"><h1>Online blood donation</h1></a>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
@@ -20,3 +18,5 @@
       </ul>
 </div>
 </nav>
+</head>
+</html>

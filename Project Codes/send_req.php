7bafbbd6de16faa9edff31c34247aee6d6e77817
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-23 17:09:16 +0600

    send_req page updated

--- Project Codes/send_req.php
+++ Project Codes/send_req.php
@@ -2,7 +2,33 @@
 <html lang="en">
  <head>
  	<title>Online Blood Bank</title>
- 	<?php include ('header.php'); ?>
+
+ <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php"><div class="logo">
+      <h1><a href="index.php"><img src="img/1lo.jpg" height="150" width="150" ; alt=""></a></h1>
+    </div></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home &ensp;<span class="sr-only"></span></a></li>
+        <a class="nav-link" href="login.php">Log in&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="registration.php">Become a donor&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="send_req.php">Send Request for blood&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="view_req.php">View Request&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="search.php">Search for blood&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="contact_us.php">Contact Us&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="about_us.php">About Us&ensp;&ensp;&emsp;&emsp;<span class="sr-only">(current)</span></a></li>
+      </ul>
+</div>
+</nav>	
+ 	
  </head>
  	<div style="height:00px;">
      <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 900px; background-color:#ffffff;">
@@ -81,10 +107,35 @@
 <input type="submit" value="Submit" name="sbmt" style="border:0px; background:red; width:100px; height:30px; border-radius:10px 1px 10px; box-shadow:1px 1px 5px black; color:black; font-weight:bold; font-size:14px; text-shadow:1px 1px 6px black; "></td></tr></table>
  
 
- </body>
+ <div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+  <div class="f_nav">
+    <ul>
+      <li class="active"><a href="index.php">Home</a></li>      
+       <li ><a href="login.php">Log in</a></li>
+        <li ><a href="registration.php">Become a donor</a></li>
+        <li ><a href="send_req.php">Send Request for blood</a></li>
+        <li ><a href="view_req.php">View Request</a></li>
+        <li ><a href="search.php">Search for blood</a></li>
+        <li ><a href="contact_us.php">Contact Us</a></li>
+        <li ><a href="about_us.php">About Us</a></li>
+      
+            </ul>
+  </div>
+    <div class="copy">
+      <p class="title"><br><br>©CSE327 Group Blood Moon  </p>
+    </div>
+  <div class="clear"></div>
+</div>
+</div>
+</div>
 
 
-<?php include ('footer.php'); ?>
+
+
+</body>
 
 </html>
 
@@ -124,5 +175,73 @@ if(isset($_POST["sbmt"]))
 	}
 		
 		}	
+
+
+
+
+<style>
+  
+.ftr-bg{
+  background: #E9DCC9;
+  border-top: 1px solid rgba(214, 195, 167, 0.34);
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+.footer{
+  padding:3% 0;
+}
+.f_nav{
+  float:left;
+}
+.f_nav li{
+  display: inline-block;
+}
+.f_nav li.active a{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+}
+.f_nav li a{
+  border-radius: 5px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  -o-border-radius: 5px;
+  display:block;
+  padding: 8px 18px;
+  color: #805752;
+  font-size: 0.8125em;
+}
+.f_nav li a:hover{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+  -webkit-transition: 0.9s;
+  -moz-transition: 0.9s;
+  -o-transition: 0.9s;
+}
+
+
+
+
+
+
+
+</style>		
 	
+
+
 

04149747c619e3820bf43187dabaa7157143021c
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-23 13:43:41 +0600

    Index and about us page header and footer adder, footer design done

--- Project Codes/index.php
+++ Project Codes/index.php
@@ -1,16 +1,132 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Online Blood Bank</title>
+  </head>
 
-<!DOCTYPE html>
-<html>
-<head>
-  <?php include 'header.php' ?>
-</head>
-<body>
- <p>
-  <b><font size="8">Welcome</font></b>
-</p><br><br><br><br><br><br>
-<font size="3">Your donotion may save one life. Be a part of this noble cause.</font>
+   <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php"><div class="logo">
+      <h1><a href="index.php"><img src="img/1lo.jpg" height="150" width="150" ; alt=""></a></h1>
+    </div></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home &ensp;<span class="sr-only"></span></a></li>
+        <a class="nav-link" href="login.php">Log in&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="registration.php">Become a donor&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="send_req.php">Send Request for blood&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="view_req.php">View Request&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="search.php">Search for blood&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="contact_us.php">Contact Us&ensp;<span class="sr-only">(current)</span></a></li>
+        <a class="nav-link" href="about_us.php">About Us&ensp;&ensp;&emsp;&emsp;<span class="sr-only">(current)</span></a></li>
+      </ul>
+</div>
+</nav>
+    <body>
+       
+  
+      <h1><span class="bold">About Our Organisation</span></h1><br><br>
+      
+    <p> 
+      Welcome to our online blood bank system.Our Website is Completely non-profit, non-political and
+        A serviceable Website.Its an attempt to work for humanity.If you are a donor,We appreciate you signing up online as a Donor.
+        If you need blood we are happy to serve you.people get trouble for blood and sometimes its run out to dealth.We believe if you want to help someone its better to give blood and save life.
+      </p>
+      
+       
+<div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+  <div class="f_nav">
+    <ul>
+      <li class="active"><a href="index.php">Home</a></li>      
+      <li><a href="donar.php">Donor</a></li>
+            <li><a href="login.php">log In</a></li>
+            <li><a href="aboutus.php">About</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+      
+            </ul>
+  </div>
+    <div class="copy">
+      <p class="title">© All Rights Reserved </p>
+    </div>
+  <div class="clear"></div>
+</div>
+</div>
+</div>
+
+       
 </body>
-<?php include 'footer.php' ?>
 </html>
 
+
+
+
+
+
+<style>
+  
+.ftr-bg{
+  background: #E9DCC9;
+  border-top: 1px solid rgba(214, 195, 167, 0.34);
+}
+.footer{
+  padding:3% 0;
+}
+.f_nav{
+  float:left;
+}
+.f_nav li{
+  display: inline-block;
+}
+.f_nav li.active a{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+}
+.f_nav li a{
+  border-radius: 5px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  -o-border-radius: 5px;
+  display:block;
+  padding: 8px 18px;
+  color: #805752;
+  font-size: 0.8125em;
+}
+.f_nav li a:hover{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+  -webkit-transition: 0.9s;
+  -moz-transition: 0.9s;
+  -o-transition: 0.9s;
+}
+
+
+
+
+
+
+
+</style>

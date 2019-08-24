213b688de05c923cff0acd0f27e9d635eebc9dbd
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-24 23:52:42 +0600

    Update search.php

--- Project Codes/search.php
+++ Project Codes/search.php
@@ -82,3 +82,99 @@
 </body>
 
 </html>
+
+<div class="clear"></div>
+<div class="ftr-bg">
+<div class="wrap">
+<div class="footer">
+  <div class="f_nav">
+    <ul>
+      <li class="active"><a href="index.php">Home</a></li>      
+       <li ><a href="login.php">Log in</a></li>
+        <li ><a href="registration.php">Become a donor</a></li>
+        <li ><a href="send_req.php">Send Request for blood</a></li>
+        <li ><a href="view_req.php">View Request</a></li>
+        <li ><a href="search.php">Search for blood</a></li>
+        <li ><a href="contact_us.php">Contact Us</a></li>
+        <li ><a href="about_us.php">About Us</a></li>
+      
+            </ul>
+  </div>
+    <div class="copy">
+      <p class="title"><br><br>©CSE327 Group Blood Moon  </p>
+    </div>
+  <div class="clear"></div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+<style>
+  
+.ftr-bg{
+  background: #E9DCC9;
+  border-top: 1px solid rgba(214, 195, 167, 0.34);
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+.footer{
+  padding:3% 0;
+}
+.f_nav{
+  float:left;
+}
+.f_nav li{
+  display: inline-block;
+}
+.f_nav li.active a{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+}
+.f_nav li a{
+  border-radius: 5px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  -o-border-radius: 5px;
+  display:block;
+  padding: 8px 18px;
+  color: #805752;
+  font-size: 0.8125em;
+}
+.f_nav li a:hover{
+  color:#ffffff;
+  background: rgb(219,60,76);
+  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
+  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
+  -webkit-transition: 0.9s;
+  -moz-transition: 0.9s;
+  -o-transition: 0.9s;
+}
+
+
+
+
+
+
+
+</style>

a6e0a080c48f502a01c2ef7d4458fa9b9daee6fe
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-25 13:50:58 +0600

    Create login.php

---
+++ Project Codes/Admin/login.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   <title>Admin login</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark" style="background-color: #4c77ed !important;">
+  <h1>Admin Panel</h1></a>
+</nav>
+</head>
+<body>
+<div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 600px;">
+      <div class="content">
+        <br><br>
+</div>

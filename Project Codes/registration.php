b294399b961e5ee257af17c292e542aefcb21f59
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-25 16:25:06 +0600

    Update registration.php

--- Project Codes/registration.php
+++ Project Codes/registration.php
@@ -1,50 +1,30 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Registration</title>
-</head>
-<body>
-	
+  <head>
+    <title>Became a Donor</title>
+   <?php include 'header.php'; ?>
+  </head>
 
-<div class="container">
-	
-    <div class="header">
-    	<h2>Register</h2>
-    </div>
+  <div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 600px;">
+      <div class="content"><br>
 
-    <form action="registration.php" method="post">
-    	
-        <div>
-        	<label for="username">User Name : </label>
-        	<input type="text" name="username" required>
-        </div>
+      <table width="100%" style="margin: 2 auto; border:3px solid;text-align:center">
+    
+    <tr><td class="lefttd"><h6>Your Name:</h6></td><td><input type="text" name="t1" required="required" title="Please enter your first name"/></td></tr><br>
+    <tr><td class="lefttd" align="center"><h6>Gender:</h6></td><td><input name="r1" type="radio" value="male" checked="checked">Male<input name="r1" type="radio" value="female">Female</td></tr><br>
+    <tr><td class="lefttd" align="center"><h6>Age:</h6></td><td><input type="number" name="t2" required="required" pattern="[0-9]{2,2}" title="please enter only  numbers between 2 to 2 for age" /></td></tr><br>
+    <tr><td class="lefttd" align="center"><h6>Blood group:</h6></td><td><input name="b1" type="radio" value="A+" checked="checked">A+<input name="b1" type="radio" value="A-">A-<input name="b1" type="radio" value="B+">B+<input name="b1" type="radio" value="B-">B-<input name="b1" type="radio" value="AB+">AB+<input name="b1" type="radio" value="AB-">AB-<input name="b1" type="radio" value="O+">O+<input name="b1" type="radio" value="O-">O-</td></tr><br>
+    <tr><td class="lefttd"><h6>Mobile no:</h6></td><td><input type="number" name="t3" required="required" pattern="[0-9]{10,11}" title="Please youe valied mobile no"/></td></tr><br>
+    <tr><td class="lefttd"><h6>Address:</h6></td><td><input type="text" name="t4" required="required" title="Please enter your address"/></td></tr><br>
+    <tr><td class="lefttd"><h6>Email:</h6></td><td><input type="email" name="t5" required="required" title="Please enter your email address"/></td></tr><br>
+    <tr><td class="lefttd"><h6>Password:</h6></td><td><input type="password" name="t6" required="required" pattern="[a-zA-Z0-9]{5,15}" title="Please enter a password in the range of 5-15 to make it strong"/></td></tr><br>
+    <tr><td class="lefttd"><h6>Confirm Password:</h6></td><td><input type="password" name="t7" required="required" pattern="[a-zA-Z0-9]{5,15}" title="Confirm it again" /></td></tr><br><br>
 
-        <div>
-        	<label for="email">Email : </label>
-        	<input type="text" name="email" required>
-        </div>
-
-         <div>
-        	<label for="password">Password : </label>
-        	<input type="password" name="password_1" required>
-        </div>
-
-         
-
-         <div>
-        	<label for="password">Confirm Password : </label>
-        	<input type="password" name="password_2"  required>
-        </div>
-
-        <button type="submit" name="reg_user">Submit</button>
-
-        <p>Already a user <a href="login.php"><b>Log in</b></a></p>
-
-    </form>
-
+    <tr><td>&nbsp;</td><td><input type="submit" value="Sign Up" name="sbmt" style="border:0px; background: red; width:125px; height:40px; box-shadow:5px 5px 10px black; color:white; font-weight:bold; font-size:20px;"></td></tr></table><br><br><br><br><br><br>
 </div>
-
-
+</form>
+</div>
+</nav>
 </body>
-</html> 
+</body>

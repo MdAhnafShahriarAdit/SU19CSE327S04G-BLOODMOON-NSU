39fee27489f5c6743e5e6d234c67bb6f3aba90f1
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-25 16:14:40 +0600

    Update login.php

--- Project Codes/login.php
+++ Project Codes/login.php
@@ -1,44 +1,22 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Log In</title>
-</head>
-<body>
+  <head>
+    <title>Login</title>
     
+    <?php include 'header.php'; ?>
 
-<div class="container">
+  </head>
     
-    <div class="header">
-        <h2>Log In</h2>
-    </div>
+  <div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 600px;">
+      <div class="content">
+        <br>
+      <table width="100%" style="margin: 0 auto; border:3px solid;text-align:center">
 
-    <form action="registration.php" method="post">
-        
-        <div>
-            <label for="username">User Name : </label>
-            <input type="text" name="username"  required>
-        </div>
+      <tr><td class="lefttd"><h3>E-Mail:</h3></td><td><input type="email" name="t1" required="required" title="Please enter your email"/></td></tr>
+      <tr><td class="lefttd"><h3>Password:</h3></td><td><input type="password"name="t2"  required="required" pattern="[a-zA-Z0-9]{2,10}" title="please enter only character or numbers between 2 to 10 for password"  /></td></tr>
 
-       
+      <tr><td>&nbsp;</td><td><input type="submit" value="Log In" name="sbmt" style="border:0px; background:#2D7FDE; width:125px; height:40px;  box-shadow:2px 2px 10px black; color:#E2ECF8; font-weight:bold; font-size:20px;"></td></tr><br>
 
-         <div>
-            <label for="password">Password : </label>
-            <input type="password" name="password_1" required>
-        </div>
-
-         
-
-         
-
-        <button type="submit" name="login_user">Log In</button>
-
-        <p>Already a user <a href="registration.php"><b>Register Here</b></a></p>
-
-    </form>
-
-</div>
-
-
-</body>
-</html>
+        <font size="5" align="center">&nbsp;&nbsp;Not a donor?<a href="registration.php" style="color:red">&nbsp;Click here</a>&nbsp;to register.</font>
+        <br><br>

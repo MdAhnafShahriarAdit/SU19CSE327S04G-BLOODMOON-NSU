94b51051108e9ea21c8a7dcf6fd39154bf5c4837
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-25 15:37:47 +0600

    Update view request page

--- Project Codes/Donor/view_req.php
+++ Project Codes/Donor/view_req.php
@@ -10,5 +10,27 @@
  <tr style="background-color:#66dea4" align="center" class="bold">            
              <td class="bold" align="center" style="color:#B40404">ID</td><td align="center" style="color:#B40404">Name</td><td align="center"style="color:#B40404">Mobile</td><td align="center" style="color:#B40404">Blood group</td>
         </tr>
-        
+        <?php
+
+$cn=mysqli_connect("localhost","root","","test");
+$s="select * from requests";
+  $result=mysqli_query($cn,$s);
+  $r=mysqli_num_rows($result);
+  
+  while($data=mysqli_fetch_array($result))
+  {
+        echo"<tr><td align='center' style='color:#380B61'>$data[0]</td><td align='center' style='color:#380B61'>$data[1]</td><td align='center' style='color:#380B61'>$data[4]</td><td align='center' style='color:#380B61'>$data[5]</td></tr>";
+      }
+      mysqli_close($cn);
+      ?>
+
+
+      </table>
+    </nav>
+  </body>
+</body><br><br><br>
 <?php include 'footer.php'; ?>
+
+
+</html>
+        

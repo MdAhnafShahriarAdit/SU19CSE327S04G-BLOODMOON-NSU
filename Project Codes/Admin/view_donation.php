a027fb6a36a09700ba5e6251e97abfb96f85f946
shamsunnaharsumi <51694797+shamsunnaharsumi@users.noreply.github.com>
2019-08-28 01:30:16 +0600

    Update view donation page under admin

---
+++ Project Codes/Admin/view_donation.php
@@ -0,0 +1,16 @@
+<html lang="en">
+  <head>
+    <title>View Request</title>
+<?php include 'header.php'; ?>
+</head>
+
+<div style="height:00px;">
+     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 900px; background-color:#c3e5eb;">
+      <div class="content"><br>
+<table cellpadding="20" cellspacing="20" width="1000px" height="200px"  style="margin:auto" >
+
+   <tr><td colspan="7" align="center"></td></tr> 
+
+   <tr><td>&nbsp;</td><td>&nbsp;</td></tr>   
+   <tr style="background-color:#6247b5" align="center" class="bold">            
+             <td class="bold" align="center" style="color:#ffffff">Name</td><td align="center" style="color:#ffffff">Mobile</td><td align="center"style="color:#ffffff">Email</td><td align="center" style="color:#ffffff">Request ID</td><td align="center" style="color:#ffffff">Blood Quantity</td></tr>        

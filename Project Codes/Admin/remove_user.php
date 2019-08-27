6368ed62a692576e57a1744e5857aa8fb2c4697c
AnimeshMondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-28 03:42:53 +0600

    Update remove_user.php

--- Project Codes/Admin/remove_user.php
+++ Project Codes/Admin/remove_user.php
@@ -31,7 +31,38 @@ $s="select * from donorregistration";
 	mysqli_close($cn);
 ?>
 </select>
-</td>
-</tr>
+<?php
+if(isset($_POST["show"]))
+{
+$cn=makeconnection();
+$s="select * from users where name='" .$_POST["s2"] ."'";
+	$result=mysqli_query($cn,$s);
+	$r=mysqli_num_rows($result);
+	//echo $r;
+	$data=mysqli_fetch_array($result);
+	$donor_id=$data[0];
+	$name=$data[1];
+	$gender=$data[2];
+	$age=$data[3];
+	$bgroup=$data[4];
+	$mobile=$data[5];
+	$address=$data[6];
+	$email=$data[7];
+	$pwd=$data[8];	
+	mysqli_close($cn);
+}
+?>
+
+</td></tr>
+<tr><td>&nbsp;</td><td><input type="submit" value="DELETE" name="sbmt"></td></tr>
 </table>
 </form>
+</div>
+</div>
+</center>
+<?php include('footer.php'); ?>
+   
+</body>
+</html>
+
+<! Taken Help from youtube and w3school !>

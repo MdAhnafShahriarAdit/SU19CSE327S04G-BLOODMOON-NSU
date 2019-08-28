136716ec819f2135933a59fc07c57d57d07154c5
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-28 21:59:29 +0600

    Fixing conflicts created by GITHUB  Desktop App

--- Project Codes/registration.php
+++ Project Codes/registration.php
@@ -1,20 +1,22 @@
-
 <!doctype html>
 <html lang="en">
   <head>
     <title>Became a Donor</title>
+   
+  </head>
 
-   <body style="background-color:">
-
-   <div class="header">
+  <div class="header">
     <h2>Online Blood Bank</h2>
   </div>
-  <div class="container">
-<div>
+
   <div class="logo">
-      <h1><a href="login.php"><img src="img/1lo.jpg" height="150" width="150" ; alt=""></a></h1>
+      <h1><a href="registration.php"><img src="img/1lo.jpg" height="150" width="150" ; alt=""></a></h1>
     </div>
-  </head>
+
+    </div>
+  <p>
+    <img src="Image/AboutUsImage.jpg" alt="">
+  </p>
 
 </div>
 
@@ -22,25 +24,23 @@
   <div style="">
      <form method="post" enctype="multipart/form-data" style="width: 1235px; height: ;">
       
-      <center><h3>Join Us!</h3></center>
-
-  <div style="height:00px;">
-     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 600px;">
-      
-
-      <table width="100%" style="margin: 2 auto; text-align:center">
-    
+      <center><h1>Join Us!</h1></center>
+      <table width="50%" style=";text-align:center">
     <tr><td class="lefttd"><h6>Your Name:</h6></td><td><input type="text" name="t1" required="required" title="Please enter your first name"/></td></tr>
     <tr><td class="lefttd" align="center"><h6>Gender:</h6></td><td><input name="r1" type="radio" value="male" checked="checked">Male<input name="r1" type="radio" value="female">Female</td></tr>
     <tr><td class="lefttd" align="center"><h6>Age:</h6></td><td><input type="number" name="t2" required="required" pattern="[0-9]{2,2}" title="please enter only  numbers between 2 to 2 for age" /></td></tr>
-    <tr><td class="lefttd" align="center"><h6>Blood group:</h6></td><td><input name="b1" type="radio" value="A+" checked="checked">A+<input name="b1" type="radio" value="A-">A-<input name="b1" type="radio" value="B+">B+<input name="b1" type="radio" value="B-">B-<input name="b1" type="radio" value="AB+">AB+<input name="b1" type="radio" value="AB-">AB-<input name="b1" type="radio" value="O+">O+<input name="b1" type="radio" value="O-">O-</td></tr>
+    
     <tr><td class="lefttd"><h6>Mobile no:</h6></td><td><input type="number" name="t3" required="required" pattern="[0-9]{10,11}" title="Please youe valied mobile no"/></td></tr>
     <tr><td class="lefttd"><h6>Address:</h6></td><td><input type="text" name="t4" required="required" title="Please enter your address"/></td></tr>
     <tr><td class="lefttd"><h6>Email:</h6></td><td><input type="email" name="t5" required="required" title="Please enter your email address"/></td></tr>
     <tr><td class="lefttd"><h6>Password:</h6></td><td><input type="password" name="t6" required="required" pattern="[a-zA-Z0-9]{5,15}" title="Please enter a password in the range of 5-15 to make it strong"/></td></tr>
-    <tr><td class="lefttd"><h6>Confirm Password:</h6></td><td><input type="password" name="t7" required="required" pattern="[a-zA-Z0-9]{5,15}" title="Confirm it again" /></td></tr><br><br>
+    <tr><td class="lefttd"><h6>Confirm Password:</h6></td><td><input type="password" name="t7" required="required" pattern="[a-zA-Z0-9]{5,15}" title="Confirm it again" /></td></tr>
 
     <tr><td>&nbsp;</td><td><input type="submit" value="Sign Up" name="sbmt" style="border:0px; background: red; width:125px; height:40px; box-shadow:5px 5px 10px black; color:white; font-weight:bold; font-size:20px;"></td></tr></table><br><br><br><br><br><br>
+
+
+     <font size="5" align="center">&nbsp;&nbsp;Already a Member?<a href="login.php" style="color:red">&nbsp;Click here</a>&nbsp;to Sign In.</font>
+        <br><br>
 
 </div>
 </form>
@@ -48,37 +48,6 @@
 </nav>
 </body>
 </body>
-
-
-
-   <?php include 'header.php'; ?>
-  </head>
-
-  <div style="height:00px;">
-     <form method="post" enctype="multipart/form-data" style="width: 1519px; height: 600px;">
-      <div class="content"><br>
-
-      <table width="100%" style="margin: 2 auto; border:3px solid;text-align:center">
-    
-    <tr><td class="lefttd"><h6>Your Name:</h6></td><td><input type="text" name="t1" required="required" title="Please enter your first name"/></td></tr><br>
-    <tr><td class="lefttd" align="center"><h6>Gender:</h6></td><td><input name="r1" type="radio" value="male" checked="checked">Male<input name="r1" type="radio" value="female">Female</td></tr><br>
-    <tr><td class="lefttd" align="center"><h6>Age:</h6></td><td><input type="number" name="t2" required="required" pattern="[0-9]{2,2}" title="please enter only  numbers between 2 to 2 for age" /></td></tr><br>
-    <tr><td class="lefttd" align="center"><h6>Blood group:</h6></td><td><input name="b1" type="radio" value="A+" checked="checked">A+<input name="b1" type="radio" value="A-">A-<input name="b1" type="radio" value="B+">B+<input name="b1" type="radio" value="B-">B-<input name="b1" type="radio" value="AB+">AB+<input name="b1" type="radio" value="AB-">AB-<input name="b1" type="radio" value="O+">O+<input name="b1" type="radio" value="O-">O-</td></tr><br>
-    <tr><td class="lefttd"><h6>Mobile no:</h6></td><td><input type="number" name="t3" required="required" pattern="[0-9]{10,11}" title="Please youe valied mobile no"/></td></tr><br>
-    <tr><td class="lefttd"><h6>Address:</h6></td><td><input type="text" name="t4" required="required" title="Please enter your address"/></td></tr><br>
-    <tr><td class="lefttd"><h6>Email:</h6></td><td><input type="email" name="t5" required="required" title="Please enter your email address"/></td></tr><br>
-    <tr><td class="lefttd"><h6>Password:</h6></td><td><input type="password" name="t6" required="required" pattern="[a-zA-Z0-9]{5,15}" title="Please enter a password in the range of 5-15 to make it strong"/></td></tr><br>
-    <tr><td class="lefttd"><h6>Confirm Password:</h6></td><td><input type="password" name="t7" required="required" pattern="[a-zA-Z0-9]{5,15}" title="Confirm it again" /></td></tr><br><br>
-
-    <tr><td>&nbsp;</td><td><input type="submit" value="Sign Up" name="sbmt" style="border:0px; background: red; width:125px; height:40px; box-shadow:5px 5px 10px black; color:white; font-weight:bold; font-size:20px;"></td></tr></table><br><br><br><br><br><br>
-
-</div>
-</form>
-</div>
-</nav>
-</body>
-</body>
-<?php include 'footer.php' ?>
 
 
 <?php include 'DBconnection.php'; ?>
@@ -100,6 +69,5 @@ if(isset($_POST["sbmt"]))
   }
     
     } 
-
 ?> 
 </html>

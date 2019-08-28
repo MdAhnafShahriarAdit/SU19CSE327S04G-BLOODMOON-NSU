46f1aebc8e720286869fdd4f0e03cec3efa7321d
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-29 01:58:21 +0600

    Form Added in The contact us page

--- Project Codes/contact_us.php
+++ Project Codes/contact_us.php
@@ -1,141 +1,68 @@
-<!Doctype html>
-<html lang="en">
-  <head>
-    <title>Online Blood Bank</title>
-  </head>
 
-   <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php"><div class="logo">
-      <h1><a href="index.php"><img src="img/1lo.jpg" height="150" width="150" ; alt=""></a></h1>
-    </div></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<?php include('header.php'); ?>
 
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <div>
+<div style="height:500px; width:100%; margin:auto; margin-top:0px; margin-bottom:125px; background-color:#f000; border:2px solid #f000; box-shadow:4px 1px 20px black;">
+     <form method="post" enctype="multipart/form-data">
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home &ensp;<span class="sr-only"></span></a></li>
-        <a class="nav-link" href="login.php">Log in&ensp;<span class="sr-only">(current)</span></a></li>
-        <a class="nav-link" href="registration.php">Become a donor&ensp;<span class="sr-only">(current)</span></a></li>
-        <a class="nav-link" href="send_req.php">Send Request for blood&ensp;<span class="sr-only">(current)</span></a></li>
-        <a class="nav-link" href="view_req.php">View Request&ensp;<span class="sr-only">(current)</span></a></li>
-        <a class="nav-link" href="search.php">Search for blood&ensp;<span class="sr-only">(current)</span></a></li>
-        <a class="nav-link" href="contact_us.php">Contact Us&ensp;<span class="sr-only">(current)</span></a></li>
-        <a class="nav-link" href="about_us.php">About Us&ensp;&ensp;&emsp;&emsp;<span class="sr-only">(current)</span></a></li>
-      </ul>
-</div>
-</nav>
-    <body>
 
-      
+  <div   class="col span_2_of_3"> <div class="contact-form" style="padding-left:100px;">
+            <img src="img/c1.jpg" height="100px" width="300px" align="center" />
   
-			<h1><span class="bold">Contact us </span></h1><br><br>
-      <p> 
-         <font sizr="5">For more information:</font><br>
-         <font sizr="4">Call us : 017********</font><br>
-         <font sizr="4">Email us : onlinebloodbank@******.com</font><br>
-         <font sizr="4">Facebook Page: www.facebook.com/onlinebloodbank</font><br>
+    <table  cellspacing="0" cellpadding="0"  width="500px" height="300px">               
 
-      </p>
+<tr><td class="lefttd"  style="vertical-align:middle"> Name:</td><td><input type="text" name="t1"  required="required" pattern="[a-zA-Z _]{2,15}" title="please enter only character  between 2 to 15 for donor name" /></td></tr>
 
 
 
+<tr><td class="lefttd"  style="vertical-align:middle">E-Mail</td><td><input type="email" name="t2" required="required" /></td></tr>
+<tr><td class="lefttd"  style="vertical-align:middle">Mobile No</td><td><input type="number" name="t3"   pattern="[0-9]{10,12}" title="please enter only numbers between 10 to 12 for mobile no." /></td></tr>
 
+<tr><td class="lefttd"  style="vertical-align:middle">Massage</td><td><textarea name="t4"></textarea></td></tr>
 
- <div class="clear"></div>
-<div class="ftr-bg">
-<div class="wrap">
-<div class="footer">
-  <div class="f_nav">
-    <ul>
-      <li class="active"><a href="index.php">Home</a></li>      
-       <li ><a href="login.php">Log in</a></li>
-        <li ><a href="registration.php">Become a donor</a></li>
-        <li ><a href="send_req.php">Send Request for blood</a></li>
-        <li ><a href="view_req.php">View Request</a></li>
-        <li ><a href="search.php">Search for blood</a></li>
-        <li ><a href="contact_us.php">Contact Us</a></li>
-        <li ><a href="about_us.php">About Us</a></li>
-      
-            </ul>
-  </div>
-    <div class="copy">
-      <p class="title"><br><br>©CSE327 Group Blood Moon  </p>
-    </div>
-  <div class="clear"></div>
+<tr><td>&nbsp;</td><td><input type="submit" value="Send Us" name="sbmt"></td></tr></table>
+
 </div>
 </div>
-</div>
+    
+      </div>
+</form>
+        </div>
+         </div> 
+         </div>
+        
+
+
+<?php
+if(isset($_POST["sbmt"])) 
+{
   
+  $cn=makeconnection();     
+
+      $s="insert into contacts(name,email,mobile,subj) values('" . $_POST["t1"] ."','" . $_POST["t2"] . "','" . $_POST["t3"] . "','" . $_POST["t4"]   ."')";
       
-       
+      
+  $q=mysqli_query($cn,$s);
+  mysqli_close($cn);
+  if($q>0)
+  {
+  echo "<script>alert('Record Save');</script>";
+  }
+  else
+  {echo "<script>alert('Saving Record Failed');</script>";
+  }
+    
+    } 
+  
+
+?> 
+
+
+ 
+
+
 </body>
+<?php include('footer.php'); ?>
 </html>
 
 
-<style>
-  
-.ftr-bg{
-  background: #E9DCC9;
-  border-top: 1px solid rgba(214, 195, 167, 0.34);
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-}
-.footer{
-  padding:3% 0;
-}
-.f_nav{
-  float:left;
-}
-.f_nav li{
-  display: inline-block;
-}
-.f_nav li.active a{
-  color:#ffffff;
-  background: rgb(219,60,76);
-  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
-  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
-  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
-  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
-  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
-  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
-}
-.f_nav li a{
-  border-radius: 5px;
-  -webkit-border-radius: 5px;
-  -moz-border-radius: 5px;
-  -o-border-radius: 5px;
-  display:block;
-  padding: 8px 18px;
-  color: #805752;
-  font-size: 0.8125em;
-}
-.f_nav li a:hover{
-  color:#ffffff;
-  background: rgb(219,60,76);
-  background: -moz-linear-gradient(top,  rgba(219,60,76,1) 0%, rgba(197,54,68,1) 56%, rgba(162,44,56,1) 99%);
-  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(219,60,76,1)), color-stop(56%,rgba(197,54,68,1)), color-stop(99%,rgba(162,44,56,1)));
-  background: -webkit-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
-  background: -o-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
-  background: -ms-linear-gradient(top,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
-  background: linear-gradient(to bottom,  rgba(219,60,76,1) 0%,rgba(197,54,68,1) 56%,rgba(162,44,56,1) 99%);
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#db3c4c', endColorstr='#a22c38',GradientType=0 );
-  -webkit-transition: 0.9s;
-  -moz-transition: 0.9s;
-  -o-transition: 0.9s;
-}
-
-
-
-
-
-
-
-</style>

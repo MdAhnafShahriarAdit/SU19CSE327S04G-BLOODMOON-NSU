f92558c33e99eece6084988b2a9c174f07a5249d
Md . Ahnaf Shahriar <51367124+MdAhnafShahriarAdit@users.noreply.github.com>
2019-08-28 22:30:27 +0600

    Chnage and position changed

--- Project Codes/header.php
+++ Project Codes/header.php
@@ -5,7 +5,9 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+    
+  <nav class="navbar navbar-expand-lg ">
+    
   <a class="navbar-brand" href="index.php"><div class="logo">
       <h1><a href="index.php"><img src="img/1lo.jpg" height="150" width="150" ; alt=""></a></h1>
     </div></a>
@@ -13,11 +15,12 @@
 
     <span class="navbar-toggler-icon"></span>
   </button>
-
+    
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
+      <li class="nav-item ">
         <a class="nav-link" href="index.php">Home &ensp;<span class="sr-only"></span></a></li>
+        <a class="nav-link" href="Facts.php">Facts About Blood Donation<span class="sr-only"></span></a></li>    
         <a class="nav-link" href="login.php">Log in&ensp;<span class="sr-only">(current)</span></a></li>
         <a class="nav-link" href="registration.php">Become a donor&ensp;<span class="sr-only">(current)</span></a></li>
         <a class="nav-link" href="send_req.php">Send Request for blood&ensp;<span class="sr-only">(current)</span></a></li>
@@ -27,6 +30,10 @@
         <a class="nav-link" href="about_us.php">About Us&ensp;&ensp;&emsp;&emsp;<span class="sr-only">(current)</span></a></li>
       </ul>
 </div>
+</div>
 </nav>
+
 </head>
 </html>
+
+

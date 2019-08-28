71fb0cc28c551e3d7ab1d2a6d8967d3fff7a7b83
Animesh Mondol <51692385+AnimeshMondol@users.noreply.github.com>
2019-08-28 21:54:08 +0600

    Update send_req.php

--- Project Codes/send_req.php
+++ Project Codes/send_req.php
@@ -143,7 +143,7 @@
 <?php
 function makeconnection()
 {
-	$cn=mysqli_connect("localhost","root","","DB name");
+	$cn=mysqli_connect("localhost","root","","test");
 	if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
